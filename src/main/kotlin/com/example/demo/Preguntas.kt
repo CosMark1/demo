7f037e5b34b91b0d87471a20709bd65f72d6380c
CosMark1 <marber24@alu.centroafuera.es>
2022-02-03 13:55:43 +0100

    o

--- src/main/kotlin/com/example/demo/Preguntas.kt
+++ src/main/kotlin/com/example/demo/Preguntas.kt
@@ -2,7 +2,7 @@ package com.example.demo
 
 import com.google.gson.Gson
 
-data class Preguntas (var Npregunta :String,var pregunta :String, var respuestas:String){
+data class Preguntas (var Npregunta :String,var pregunta :String, var respuesta1:String, var respuesta2:String, var respuesta3:String, var respuesta4:String){
 
     override  fun toString() : String{
         val gson = Gson()
--- src/main/kotlin/com/example/demo/Repository.kt
+++ src/main/kotlin/com/example/demo/Repository.kt
@@ -17,16 +17,16 @@ class Repository {
         )
         var listaPreguntas = listOf(
 
-            Preguntas("Pregunta 1","Capital de España","Madrid - Paris - Bélgica - Atenas"),
-            Preguntas("Pregunta 2","Capital de Francia","Moscú - Paris - Bucarest - Lisboa"),
-            Preguntas("Pregunta 3","Capital de Alemania","Bélgica - Moscú - Washington D.C - Tokio"),
-            Preguntas("Pregunta 4","Capital de Rusia","Moscú - Atenas - Pekín - Bucarest"),
-            Preguntas("Pregunta 5","Capital de Grecia","Atenas - Lisboa - Washington D.C - Madrid"),
-            Preguntas("Pregunta 6","Capital de EEUU","Washington D.C - Bucarest - Lisboa - Pekin"),
-            Preguntas("Pregunta 7","Capital de Portugal","Lisboa - Bélgica - Paris - Washington D.C"),
-            Preguntas("Pregunta 8","Capital de Japón","Tokio - Paris - Washington D.C - lisboa"),
-            Preguntas("Pregunta 9","Capital de China","Paris - Pekín - Bélgica - Bucarest"),
-            Preguntas("Pregunta 10","Capital de Rumania","Paris - Washington D.C - Bucarest - Atenas")
+            Preguntas("Pregunta 1","Capital de España","Madrid","Paris","Bélgica","Atenas"),
+            Preguntas("Pregunta 2","Capital de Francia","Moscú","Paris","Bucarest", "Lisboa"),
+            Preguntas("Pregunta 3","Capital de Alemania","Bélgica","Moscú" , "Washington D.C" ,"Tokio"),
+            Preguntas("Pregunta 4","Capital de Rusia","Moscú" , "Atenas" , "Pekín" ,"Tokio"),
+            Preguntas("Pregunta 5","Capital de Grecia","Atenas" , "Lisboa" , "Washington D.C" ,"Madrid"),
+            Preguntas("Pregunta 6","Capital de EEUU","Washington D.C" , "Bucarest" , "Lisboa" , "Pekin"),
+            Preguntas("Pregunta 7","Capital de Portugal","Lisboa" , "Bélgica" , "Paris" , "Washington D.C"),
+            Preguntas("Pregunta 8","Capital de Japón","Tokio", "Paris" , "Washington D.C" , "lisboa"),
+            Preguntas("Pregunta 9","Capital de China","Paris" , "Pekín" , "Bélgica" ,"Bucarest"),
+            Preguntas("Pregunta 10","Capital de Rumania","Paris" , "Washington D.C" , "Bucarest" , "Atenas")
         )
     }
 }
