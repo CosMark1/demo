bf11c45427e4ea60f027c667fc1c47b5a7a7115d
CosMark1 <marber24@alu.centroafuera.es>
2022-02-03 14:24:49 +0100

    o

--- src/main/kotlin/com/example/demo/Repository.kt
+++ src/main/kotlin/com/example/demo/Repository.kt
@@ -17,16 +17,16 @@ class Repository {
         )
         var listaPreguntas = listOf(
 
-            Preguntas("Pregunta 1","Capital de España","Madrid","Paris","Bélgica","Atenas"),
-            Preguntas("Pregunta 2","Capital de Francia","Moscú","Paris","Bucarest", "Lisboa"),
-            Preguntas("Pregunta 3","Capital de Alemania","Bélgica","Moscú" , "Washington D.C" ,"Tokio"),
-            Preguntas("Pregunta 4","Capital de Rusia","Moscú" , "Atenas" , "Pekín" ,"Tokio"),
-            Preguntas("Pregunta 5","Capital de Grecia","Atenas" , "Lisboa" , "Washington D.C" ,"Madrid"),
-            Preguntas("Pregunta 6","Capital de EEUU","Washington D.C" , "Bucarest" , "Lisboa" , "Pekin"),
-            Preguntas("Pregunta 7","Capital de Portugal","Lisboa" , "Bélgica" , "Paris" , "Washington D.C"),
-            Preguntas("Pregunta 8","Capital de Japón","Tokio", "Paris" , "Washington D.C" , "lisboa"),
-            Preguntas("Pregunta 9","Capital de China","Paris" , "Pekín" , "Bélgica" ,"Bucarest"),
-            Preguntas("Pregunta 10","Capital de Rumania","Paris" , "Washington D.C" , "Bucarest" , "Atenas")
+            Preguntas(0,"Capital de España","Madrid","Paris","Bélgica","Atenas"),
+            Preguntas(1,"Capital de Francia","Moscú","Paris","Bucarest", "Lisboa"),
+            Preguntas(2,"Capital de Alemania","Bélgica","Moscú" , "Washington D.C" ,"Tokio"),
+            Preguntas(3,"Capital de Rusia","Moscú" , "Atenas" , "Pekín" ,"Tokio"),
+            Preguntas(4,"Capital de Grecia","Atenas" , "Lisboa" , "Washington D.C" ,"Madrid"),
+            Preguntas(5,"Capital de EEUU","Washington D.C" , "Bucarest" , "Lisboa" , "Pekin"),
+            Preguntas(6,"Capital de Portugal","Lisboa" , "Bélgica" , "Paris" , "Washington D.C"),
+            Preguntas(7,"Capital de Japón","Tokio", "Paris" , "Washington D.C" , "lisboa"),
+            Preguntas(8,"Capital de China","Paris" , "Pekín" , "Bélgica" ,"Bucarest"),
+            Preguntas(9,"Capital de Rumania","Paris" , "Washington D.C" , "Bucarest" , "Atenas")
         )
     }
 }
